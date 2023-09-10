4313f870afe856c1b49cc9d8bf075790838dba7e
Larina <49904492+Larinax999@users.noreply.github.com>
2023-09-11 06:12:14 +0700

    ✨0

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+author "larinax999"
+description "Huffman algorithm"
+
+client_scripts {
+    "lib.lua",
+    "client.lua"
+}
+server_scripts {
+    "lib.lua",
+    "server.lua"
+}
+
+lua54 'yes'
+fx_version "adamant"
+game "gta5"
